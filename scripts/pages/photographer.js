c9a8363120b6fd1dc0ac5183f8d21726543c03e4
Yuliya BERT <98463848+ElkaXX@users.noreply.github.com>
2024-01-23 13:33:44 +0100

    Téléchargé et utilisé des exemples de photos et de videos

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,44 @@
-//Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographers() {
+  const response = await fetch("data/photographers.json");
+  return await response.json();
+}
+
+function displayPhotographer(photographer) {
+  const name = document.querySelector(".photograph-header__name");
+  const location = document.querySelector(".photograph-header__location");
+  const tag = document.querySelector(".photograph-header__tag");
+  const img = document.querySelector(".photograph-header__img");
+
+  name.textContent = photographer.name;
+  location.textContent = `${photographer.city}, ${photographer.country}`;
+  tag.textContent = photographer.tagline;
+  img.src = `assets/photographers/${photographer.portrait}`;
+}
+
+function displayPhotos(name, photos) {
+  const list = document.querySelector(".photograph-photos");
+
+  photos.forEach((photo) => {
+    const listItem = getPhotoListItemTemplate(name, photo);
+    list.appendChild(listItem);
+  });
+}
+
+async function init() {
+  const { photographers, media } = await getPhotographers();
+
+  const photographerId = new URL(location.href).searchParams.get("id");
+
+  const photograpther = photographers.find(
+    (element) => element.id === +photographerId
+  );
+
+  const photos = media.filter(
+    (element) => element.photographerId === +photographerId && element.image
+  );
+
+  displayPhotographer(photograpther);
+  displayPhotos(photograpther.name, photos);
+}
+
+init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,43 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { id, name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/id/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", name);
+
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+
+    const locationP = document.createElement("p");
+    locationP.textContent = `${city}, ${country}`;
+    locationP.classList.add("photographer__location");
+
+    const tagP = document.createElement("p");
+    tagP.textContent = tagline;
+    tagP.classList.add("photographer__tag");
+
+    const priceP = document.createElement("p");
+    priceP.textContent = `${price}€/jour`;
+    priceP.classList.add("photographer__price");
+
+    const link = document.createElement("a");
+    link.setAttribute("href", `photographer?id=${id}`);
+    link.classList.add("photographer__link");
+    link.appendChild(img);
+    link.appendChild(h2);
+
+    article.appendChild(link);
+    article.appendChild(locationP);
+    article.appendChild(tagP);
+    article.appendChild(priceP);
+
+    return article;
+  }
+
+  return { name, picture, getUserCardDOM };
 }
