a1c6228a2baf5f220894e8c800e72ce0023fea3c
Yuliya BERT <98463848+ElkaXX@users.noreply.github.com>
2024-02-01 17:16:58 +0100

    style de filters

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,4 +1,27 @@
-async function getPhotographers() {
+const getPhotographers = async () => {
+  const response = await fetch("data/photographers.json");
+  return await response.json();
+};
+
+const displayData = async (photographers) => {
+  const photographersSection = document.querySelector(".photographer_section");
+
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerTemplate(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+};
+
+const init = async () => {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
+};
+
+init();
+
+/*async function getPhotographers() {
   const response = await fetch("data/photographers.json");
   return await response.json();
 }
@@ -19,4 +42,4 @@ async function init() {
   displayData(photographers);
 }
 
-init();
+init();*/
